9817ded19aab47a8b194266da0d0207cf4b6668b
Rubens Marcondes <rubenspotcake@proton.me>
2025-04-15 19:52:07 -0300

    Correcao end point hello

--- src/main/java/com/rubenspotcake/hello_world_docker/HelloDockerController.java
+++ src/main/java/com/rubenspotcake/hello_world_docker/HelloDockerController.java
@@ -4,15 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/hello")
 public class HelloDockerController {
-  @RestController
-  @RequestMapping("/hello")
-  public class HelloWorldController {
-
-    @GetMapping
-    public String hello() {
-      return "Hello, Docker!";
-    }
+  @GetMapping
+  public String hello() {
+    return "Hello, Docker!";
   }
-
 }
